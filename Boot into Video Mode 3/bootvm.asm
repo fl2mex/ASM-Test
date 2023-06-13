255b75f83bdd3e566655ce2780b0ea7ef9971df2
Kaiden Bird <kaidenchrisbird@gmail.com>
2023-06-13 18:50:04 +1000

    Fixed Video Mode 3

--- Boot into Video Mode 3/bootvm.asm
+++ Boot into Video Mode 3/bootvm.asm
@@ -1,9 +1,12 @@
-; Booting into 16-bit mode with video mode 3
+; Video Mode - Text
 ; Made by Kaiden Bird
 ; 13/06/2023
 
-mov ax, 0x3 ; Set video mode to 3
-int 10h ; Call video subroutine
+use16 ; Use 16-bit mode
+org 0x7c00 ; Set the origin to 0x7c00
+
+mov ax, 0x3 ; 80x25 @ 16 color mode
+int 10h ; Video BIOS interrupt
 
 ; Legacy boot bios expects the boot sector to be 512 bytes long.
 times 510-($-$$) db 0 ; Fills 510 bytes of the boot sectors to 0.
